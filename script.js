a6563be6f7ff13cd0d7ae99f20b0130414c27ee5
Tommy Dang <ledangt310@live.com>
2022-07-12 17:15:59 -0700

    Added dice roll and active player functionality

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-'Use Strict'
+'Use Strict';
+
+//Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0')
+const current1El = document.getElementById('current--1')
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0 , 0];
+
+let currentScore = 0; //curentScore is outside the function as putting it inside would reset the score to '0' when the button is clicked.
+
+let activePlayer = 0;
+
+//rolling dice functionality
+btnRoll.addEventListener('click', function () {
+    //1. Generate random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
+
+    //2. Display dice roll
+    diceEl.classList.remove('hidden');
+    diceEl.src = `./assets/images/dice-${dice}.png`;
+
+    //3. Check for rolled '1' : If true switch to other player
+    if(dice !== 1) {
+        //add dice roll to current score
+        currentScore += dice;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    } else {
+        //switch to the next player
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
+    }
+});
+    
+   
+
+
